46c9d5d12f036abfccc24c5acc5f8d1382d24362
Nikita K <rabot422@gmail.com>
2023-09-24 19:10:28 +0300

    make range-scroll for sizing cells

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-
+const slider = document.querySelector('.slider');
 
 function setCellSize(cellSize) {
     const board = document.getElementById('board');
+
     const cols = Math.floor(board.clientWidth / cellSize);
     const rows = Math.floor(board.clientHeight / cellSize);
     let flag = false;
@@ -38,17 +39,18 @@ function setCellSize(cellSize) {
     board.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
   }
+
+
+  // // Запросите размер ячейки с помощью prompt
+  // const cellSizeInput = prompt('Введите размер ячейки (не более 100px):');
+  // const cellSize = parseInt(cellSizeInput);
   
-  // Запросите размер ячейки с помощью prompt
-  const cellSizeInput = prompt('Введите размер ячейки (не более 100px):');
-  const cellSize = parseInt(cellSizeInput);
-  
-  // Проверка, что введено число и оно больше 0 и не более 100
-  if (!isNaN(cellSize) && cellSize > 0 && cellSize <= 100) {
-    // Вызовите функцию для установки размера ячейки
-    setCellSize(cellSize);
-  } else {
-    alert('Пожалуйста, введите корректное число от 1 до 100.');
-  }
+  // // Проверка, что введено число и оно больше 0 и не более 100
+  // if (!isNaN(cellSize) && cellSize > 0 && cellSize <= 100) {
+  //   // Вызовите функцию для установки размера ячейки
+  //   setCellSize(cellSize);
+  // } else {
+  //   alert('Пожалуйста, введите корректное число от 1 до 100.');
+  // }
   
   
